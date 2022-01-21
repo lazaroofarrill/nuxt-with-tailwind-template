15aac29271e133a603c35b108c55c5c6ca810ef0
lazaroofarrill <lazaroofarrill@gmail.com>
2022-01-20 22:34:30 -0500

    getting started with tailwind

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class='bg-black text-white text-center py-6 shadow rounded'>hello</div>
+  <div
+    class='navbar opacity-80 h-20 shadow-lg bg-black text-neutral-content'>
+    <div class='navbar-start'></div>
+    <div class='navbar-center'>
+      <div class='grid grid-flow-col gap-4 '>
+        <button class='btn btn-primary ' v-for='i in 3' :key='i'>
+          <icon-hero-academic-cap />
+          I'm a link
+        </button>
+        <button class='btn bg-green-700'>Welcome to espoleta</button>
+        <button class='btn btn-primary' v-for='i in 3' :key='i'>
+          <icon-hero-academic-cap />
+          I'm a link
+        </button>
+      </div>
+    </div>
+    <div class='navbar-end'></div>
+  </div>
 </template>
 
 <script lang='ts'>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,27 @@
 <template>
-  <layout-header />
+  <div>
+    <layout-header />
+    <div style=' width: 300px' class='mt-20 bg-yellow-200'>
+      <p class='line-clamp-6 hover:line-clamp-none'>
+        Et molestiae hic earum repellat aliquid est doloribus delectus. Enim illum odio porro ut omnis dolor debitis
+        natus. Voluptas possimus deserunt sit delectus est saepe nihil. Qui voluptate possimus et quia. Eligendi
+        voluptas voluptas dolor cum. Rerum est quos quos id ut molestiae fugit.
+        Et molestiae hic earum repellat aliquid est doloribus delectus. Enim illum odio porro ut omnis dolor debitis
+        natus. Voluptas possimus deserunt sit delectus est saepe nihil. Qui voluptate possimus et quia. Eligendi
+        voluptas voluptas dolor cum. Rerum est quos quos id ut molestiae fugit.
+        Et molestiae hic earum repellat aliquid est doloribus delectus. Enim illum odio porro ut omnis dolor debitis
+        natus. Voluptas possimus deserunt sit delectus est saepe nihil. Qui voluptate possimus et quia. Eligendi
+        voluptas voluptas dolor cum. Rerum est quos quos id ut molestiae fugit.
+        Et molestiae hic earum repellat aliquid est doloribus delectus. Enim illum odio porro ut omnis dolor debitis
+        natus. Voluptas possimus deserunt sit delectus est saepe nihil. Qui voluptate possimus et quia. Eligendi
+        voluptas voluptas dolor cum. Rerum est quos quos id ut molestiae fugit.
+      </p>
+
+
+    </div>
+    <a class='btn'>Hello!</a>
+    <icon-hero-academic-cap class='text-8xl' />
+  </div>
 </template>
 
 <script lang='ts'>
